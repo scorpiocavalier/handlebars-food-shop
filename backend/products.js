507ea29b5b1f4c3f505a91d03418643c0e031324
Arturo Chen <art.can.fly@gmail.com>
2021-01-28 20:32:45 -0500

    + testing GET requests

---
+++ backend/products.js
@@ -0,0 +1,16 @@
+const Products = [
+  {
+    id: 1,
+    name: 'Soap',
+  },
+  {
+    id: 2,
+    name: 'Brush',
+  },
+  {
+    id: 3,
+    name: 'Shoe',
+  },
+];
+
+module.exports = Products;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
-const express = require('express');
+const Express = require('express');
+const Products = require('./products');
 
-const app = express();
+const app = Express();
+const port = process.env.port || 4200;
 
-app.use(express.static('frontend'));
+app.use(Express.static('/frontend'));
 
-const port = process.env.port || 4000;
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+app.get('/products', (req, res) => {
+  res.json(Products);
+});
+
+app.get('/products/:id', (req, res) => {
+  const productId = req.params.id;
+  const product = Products.find((product) => product.id == productId);
+  res.send(product.name);
+});
 
 app.listen(port, () => console.log(`Server listenning on port ${port}`));
