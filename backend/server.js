2a35312dc87c9a9df1d0f39fbb08a4c9e670ddc5
Arturo Chen <art.can.fly@gmail.com>
2021-01-30 00:33:21 -0500

    + hero and features sections

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
-const Express = require('express');
+const express = require('express');
 const Products = require('./products');
+const path = require('path');
 
-const app = Express();
+const app = express();
 const port = process.env.port || 4200;
 
-app.use(Express.static('/frontend'));
+app.use(express.static('frontend'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Hello');
+  res.sendFile('index.html');
 });
 
 app.get('/products', (req, res) => {
@@ -18,6 +21,12 @@ app.get('/products/:id', (req, res) => {
   const productId = req.params.id;
   const product = Products.find((product) => product.id == productId);
   res.send(product.name);
+});
+
+app.post('/add', (req, res) => {
+  const { id, name } = req.body;
+  console.log(id, name);
+  res.sendStatus(200);
 });
 
 app.listen(port, () => console.log(`Server listenning on port ${port}`));
